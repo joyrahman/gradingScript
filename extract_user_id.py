107e196f0b9b3de25cf68519d73d78b15dcdd006
unknown <iffat.chowdhury@utsa.edu>
2016-10-12 14:26:43 -0500

    new file added

---
+++ extract_user_id.py
@@ -0,0 +1,12 @@
+def test(input_dir, file_name_pattern):
+    for filename in os.listdir(input_dir):
+        if file_name_pattern in filename:
+            #get user id 
+            user_id = filename.split('OpenMP_')[1].split('_',1)[0]
+            print user_id
+
+
+if __name__ == "__main__":
+	input_dir =  './submission'
+    file_name_pattern = "k_means.c"
+	test(input_dir, file_name_pattern)
